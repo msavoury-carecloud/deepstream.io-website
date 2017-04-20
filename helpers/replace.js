e2af7a7a90d19e088a48a611da0133c4b554fcdc
Juliet Matsai <julietmatsai@gmail.com>
2017-04-20 10:59:38 +0200

    iOS/Swift Client SDK

--- helpers/replace.js
+++ helpers/replace.js
@@ -1,6 +1,7 @@
 const replacements = {
 	'client-js': 'JavaScript Client',
 	'client-java': 'Java/Android Client',
+	'client-swift': 'Swift/iOS Client',
 	'common': 'Shared',
 	'server': 'Server configuration'
 }
